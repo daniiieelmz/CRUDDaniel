0f0454efda7c4a30887fed7bf1e820f2458ba5ce
Paola <20230523@ricaldone.edu.sv>
2024-05-02 15:38:22 -0600

    CRUD Daniel

---
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -0,0 +1,23 @@
+package modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+
+class ClaseConexion {
+
+    fun cadenaConexion(): Connection?{
+
+        try {
+            val ip = "jdbc:oracle:thin:@10.10.3.32:xe"
+            val usuario = "system"
+            val contrasena = "desarrollo"
+
+            val conexion = DriverManager.getConnection(ip, usuario, contrasena)
+            return conexion
+        }catch (e: Exception){
+            println("El error es este: $e")
+            return null
+        }
+    }
+}
